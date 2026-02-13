c36aeed186a251db8000961f2c054a2f3d2ce182
salvadorRodriguez2 <salrodrigueztx@gmail.com>
2026-02-13 14:34:10 -0600

    This should be the final step to START this program

--- jkws-server/app.py
+++ jkws-server/app.py
@@ -2,15 +2,14 @@
     Author: Salvador Rodriguez
     Description: This will be the server running on port 8080. 
     
-    If new terminal is opened, use .venv\Scripts\Activate.ps1 to activate the environment again.
-    Then do python -m flask run -p 8080 to run the server and have it open.
-
-    I ran: pip install pyjwt[crypto]
+    Steps to run:
+        1. Activate environment: .\.venv\Scripts\Activate.ps1
+        2. Start server:         python -m flask run -h localhost -p 8080
 '''
 
 # Libraries
 from flask import Flask
-from pyjwt import Pyjwt
+import jwt
 
 app = Flask(__name__)
 
