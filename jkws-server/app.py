9c3160051f3c5d9170e8ce9e50c8c86de50ed420
salvadorRodriguez2 <salrodrigueztx@gmail.com>
2026-02-13 10:08:03 -0600

    I have added the libraries I believe needed to be added.

--- jkws-server/app.py
+++ jkws-server/app.py
@@ -1,12 +1,19 @@
 ''' 
     Author: Salvador Rodriguez
     Description: This will be the server running on port 8080. 
+    
     If new terminal is opened, use .venv\Scripts\Activate.ps1 to activate the environment again.
+    Then do python -m flask run -p 8080 to run the server and have it open.
+
+    I ran: pip install pyjwt[crypto]
 '''
 
 # Libraries
 from flask import Flask
+from pyjwt import Pyjwt
 
-@webServer.route("/")
+app = Flask(__name__)
+
+@app.route("/")
 def home():
     return "Hello, Flask!"
